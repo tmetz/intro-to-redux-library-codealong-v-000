d06fbb7d20f74aa6940dd1f93d25af8601fc494b
Tammy Metz <tammy@tammymetz.com>
2020-01-19 04:59:43 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import App from './App';
 import './index.css';
 
 const store = createStore(shoppingListItemReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && 
+  window.__REDUX_DEVTOOLS_EXTENSION__ &&
   window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
